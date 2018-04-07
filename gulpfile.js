3babbb9bc1b525309d48173e419f30dcb7725bf8
Ugo <ugo.briasco@sumup.com>
2018-04-07 22:39:11 +0200

    Uglify lib.js

--- gulpfile.js
+++ gulpfile.js
@@ -5,16 +5,19 @@ const imagemin = require('gulp-imagemin');
 const concat = require('gulp-concat');
 const babel = require('gulp-babel');
 const uglify = require('gulp-uglify');
+const pump = require('pump');
+const rename = require('gulp-rename');
 const browserSync = require('browser-sync').create();
 
 gulp.task(
   'default',
-  ['copy-html', 'styles', 'copy-img', 'dev-scripts', 'sw'],
+  ['copy-html', 'styles', 'copy-img', 'scripts', 'sw', 'compress-lib-js'],
   function() {
     gulp.watch('src/*.html', ['copy-html']);
     gulp.watch('src/scss/*.scss', ['styles']);
-    gulp.watch('src/js/*.js'), ['dev-scripts'];
+    gulp.watch('src/js/*.js'), ['scripts'];
     gulp.watch('src/sw.js'), ['sw'];
+    gulp.watch('dist/js/*.js'), ['compress-lib-js'];
     gulp.watch('scr/img/*', ['copy-img']);
     gulp.watch('./dist/index.html').on('change', browserSync.reload);
 
@@ -45,12 +48,13 @@ gulp.task('copy-img', function() {
 });
 
 gulp.task('scripts', function() {
-  gulp
-    .src('src/js/**/*.js')
+  return gulp
+    .src('src/js/lib/*.js')
     .pipe(babel())
-    .pipe(concat('all.js'))
+    .pipe(concat('lib.js'))
     .pipe(gulp.dest('./dist/js'));
 });
+
 gulp.task('dev-scripts', function() {
   gulp
     .src('src/js/*.js')
@@ -64,6 +68,21 @@ gulp.task('sw', function() {
     .pipe(babel())
     .pipe(concat('sw.js'))
     .pipe(gulp.dest('./dist'));
+});
+
+gulp.task('compress-lib-js', function(cb) {
+  pump(
+    [
+      gulp.src('dist/js/lib.js'),
+      rename('lib.min.js'),
+      babel({
+        presets: ['es2015']
+      }),
+      uglify(),
+      gulp.dest('./dist/js')
+    ],
+    cb
+  );
 });
 
 gulp.task('styles', function() {
